6104f99022dc054e9f6959f6ccb1d3e3674e5464
lizihuai <136703713@qq.com>
2018-11-15 14:21:59 +0800

    update build.sh

--- build/build.sh
+++ build/build.sh
@@ -7,7 +7,7 @@ BINARY_NAME="cse-go-chassis-demo-1.3.1"
 CURRENT_DIR=$(cd $(dirname $0);pwd)
 ROOT_PATH=$(dirname $CURRENT_DIR)
 
-DNAME=$(dirname $(dirname "$ROOT_PATH"))
+DNAME=$(dirname $(dirname $(dirname "$ROOT_PATH")))
 export  GOPATH="$DNAME"
 
 cd $ROOT_PATH
