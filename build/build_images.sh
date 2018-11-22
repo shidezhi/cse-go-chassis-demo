0181fa9fd23c1cd5b6c446a8573e3ce28ccaac7f
lizihuai <136703713@qq.com>
2018-11-22 10:48:29 +0800

    update shell

--- build/build_images.sh
+++ build/build_images.sh
@@ -9,6 +9,8 @@ cd $ROOT_PATH
 
 IMAGE="cse-go-chassis-demo-1.3.1"
 TAG="latest"
-
+if [[ "$1" != "" ]];then
+    IMAGE=$1
+fi
 docker build -t $IMAGE:$TAG .
 #docker save -o $IMAGE-$TAG.tar $IMAGE:$TAG
